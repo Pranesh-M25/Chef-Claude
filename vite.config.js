f0c5ea6fe3a8eb9680471e14a7be081ec0e2dda3
Pranesh M <94372142+Pranesh-M25@users.noreply.github.com>
2025-09-13 22:26:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // Add this line
-  base: "/Chef-Claude/", 
+  //base: "/Chef-Claude/", 
   plugins: [react()],
   base: process.env.VITE_BASE_PATH || "/Chef-Claude",
 })
