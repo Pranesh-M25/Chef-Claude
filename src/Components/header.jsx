2b23e255cc47ea7428e7f3a11c555e1d7eb23155
Pranesh M <94372142+Pranesh-M25@users.noreply.github.com>
2025-09-14 15:38:43 +0530

    Refactor Header component to remove button

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -2,7 +2,7 @@ import chefIcon from "../assets/chef.png";
 export function Header() {
   return (
     <section className="header">
-      <button className="iconBtn" onClick={() => window.location.reload()}><img className="icon" src={chefIcon} alt="Chef Icon" /></button>
+      <img className="icon" src={chefIcon} alt="Chef Icon" />
       <h1 className="title">Chef Claude</h1>
     </section>
   );
