4491d514ddaa55542d0cad7e27fec84a090126fa
Pranesh M <94372142+Pranesh-M25@users.noreply.github.com>
2025-09-14 15:11:33 +0530

    Update RecipeInstructions.jsx

--- src/Components/RecipeInstructions.jsx
+++ src/Components/RecipeInstructions.jsx
@@ -4,7 +4,7 @@ export default function Recipe({ recipeText, isLoading }) {
   return (
     <section>
       <article className="suggested-recipe-container" aria-live="polite">
-        <h2>Chef Claude Recommends:</h2>
+        <h2>Chef Claude Recommends</h2>
 
         {/* 3. Display a loading message or the recipe */}
         {isLoading && <p>Thinking of a delicious recipe for you...</p>}
@@ -19,3 +19,4 @@ export default function Recipe({ recipeText, isLoading }) {
     </section>
   );
 }
+
