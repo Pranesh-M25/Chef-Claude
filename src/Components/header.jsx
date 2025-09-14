60601f8fdd7e356c1910e689a9014b2873f4f0a1
Pranesh M <94372142+Pranesh-M25@users.noreply.github.com>
2025-09-14 15:37:03 +0530

    Add reload button to Header component

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -2,8 +2,9 @@ import chefIcon from "../assets/chef.png";
 export function Header() {
   return (
     <section className="header">
-      <img className="icon" src={chefIcon} alt="Chef Icon" />
+      <button className="iconBtn" onClick={() => window.location.reload()}><img className="icon" src={chefIcon} alt="Chef Icon" /></button>
       <h1 className="title">Chef Claude</h1>
     </section>
   );
 }
+
